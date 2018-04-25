e3b78a78f63479b8eee770b9fa20fa66a419d525
martshaw <martyshaw@hotmail.co.uk>
2018-04-25 17:49:58 +0200

    updating homepage and publications

---
+++ _next/fc7b2c33-7cd1-4117-adf1-0322d9028464/page/publications/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/publications', function() {
+            var comp = module.exports=webpackJsonp([5],{316:function(e,t,u){e.exports=u(317)},317:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(5),l=n(o),r=u(2),a=n(r),f=u(3),i=n(f),p=u(7),s=n(p),d=u(8),c=n(d),_=u(0),m=n(_),h=u(23),v=u(87),y=n(v),b=u(41),x=u(89),E=function(e){function t(){return(0,a.default)(this,t),(0,s.default)(this,(t.__proto__||(0,l.default)(t)).apply(this,arguments))}return(0,c.default)(t,e),(0,i.default)(t,[{key:"render",value:function(){return console.log("about",this.props),m.default.createElement("div",null,m.default.createElement(b.Layout,null,m.default.createElement(x.App,{page:"publications"})))}}]),t}(_.Component);t.default=(0,h.compose)(y.default,h.withApollo)(E)},55:function(e,t){e.exports=require("styletron-server")}},[316]);
+            return { page: comp.default }
+          })
+        
